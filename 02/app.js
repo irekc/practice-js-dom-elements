7c2c19cc0cab6f3a5c11d8b5dfd3ad1011222996
Ireneusz Cislo <irekc86@o2.pl>
2023-04-13 06:17:51 +0200

    task 2 finished

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,13 @@
-console.log('DOM');
+console.log('DOM'); 
+
+const elList = document.querySelectorAll('[data-url]');
+
+if(elList) {
+    elList.forEach(el => {
+        // const attr = el.getAttribute('data-url');
+        const attr = el.dataset.url;
+        el.setAttribute('href', attr);
+        console.log(el, elList)
+    }
+    )
+}
