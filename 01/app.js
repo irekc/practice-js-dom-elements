e5bfa51cf5d078bc598dad856563c5953cbd6744
Ireneusz Cislo <irekc86@o2.pl>
2023-04-13 05:53:56 +0200

    task 1 finished

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,16 @@
 console.log('DOM');
+
+// const newestComment = document.getElementsByClassName('comments__item comments__item--newest');
+
+// if(newestComment) {
+//     const CommentElements = newestComment[0].children;
+
+//     console.log(newestComment, CommentElements)
+// }
+
+const newestComment = document.querySelector('.comments__item.comments__item--newest');
+
+if(newestComment) {
+    const listWithAtrr = newestComment.querySelectorAll('[data-info]');
+    console.log(listWithAtrr,listWithAtrr.length)
+}
