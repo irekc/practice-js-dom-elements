f10a42ea2554a60cd3c028a624a06975700d397a
Ireneusz Cislo <irekc86@o2.pl>
2023-04-13 06:42:02 +0200

    task 3 finished

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,18 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const parent = document.querySelector('.parent-for-button');
+const button = document.createElement('button');
+
+if(parent) {
+    parent.appendChild(button)
+    for(const value in buttonSettings.attr) {
+        button[value] = buttonSettings.attr[value]
+    }
+    for(const value in buttonSettings.css) {
+        button.style[value] = buttonSettings.css[value]
+    }
+    button.innerText = buttonSettings.text
+
+}
