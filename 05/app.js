e1aeb324be31d8e1a0795fb60084e269fd04acfc
Ireneusz Cislo <irekc86@o2.pl>
2023-04-14 06:51:35 +0200

    task 5 finished

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,39 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const removeButton = document.createElement('button');
+removeButton.innerText = 'usuń z koszyka';
+removeButton.className = 'article__btn'
+
+const article1 = curr.parentElement;
+article1.appendChild(removeButton);
+
+const currSiblings = [...article1.children];
+console.log(currSiblings);
+
+currSiblings.forEach(el => {
+    if(el !== curr) {
+        console.log(el)
+        // el.className += ' siblings'
+        el.classList.add('siblings')
+
+    }
+})
+
+const article2 = article1.nextElementSibling;
+article2.setAttribute('title', 'nextElementSibling');
+
+const article3 = article2.nextElementSibling;
+const newP = document.createElement('p');
+newP.innerText = 'some text';
+const lastArticle3Button = article3.querySelector('.article__btn');
+article3.insertBefore(newP, lastArticle3Button);
+
+const newArticle = article2.cloneNode(true);
+const section = article1.parentElement;
+section.insertBefore(newArticle, article1)
+
+
+
+
