56619ceef3e26810d179bc335db8f301f0ba94da
Ireneusz Cislo <irekc86@o2.pl>
2023-04-14 05:53:11 +0200

    task 4 finished

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,23 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const nav = document.querySelector('nav');
+const ulElement = document.createElement('ul');
+nav.appendChild(ulElement)
+
+if(ulElement) {
+    menuItems.forEach(element => {
+        const liElement = document.createElement('li');
+        const aElement = document.createElement('a')
+        
+        aElement.innerText = element.text;
+        aElement.setAttribute('href', element.url)
+        
+        ulElement.appendChild(liElement);
+        liElement.appendChild(aElement)
+
+
+    })
+
+}
